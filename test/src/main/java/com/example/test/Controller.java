0d19de655a0894be0a7f867ec2b7834c2297701e
Malikaelabderrahmani <malikarahmani67@gmail.com>
2024-11-24 13:21:07 +0100

    test the application

---
+++ test/src/main/java/com/example/test/Controller.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
